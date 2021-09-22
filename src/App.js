f2ddd4b2ebdf97f2a4b5d6e3aa4bc78779120ae3
prabhatpathak <prabhatpathak9076@gmail.com>
2021-09-22 19:39:24 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/home';
+import Login from './components/login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/header';
+import ProductDetails from './components/ProductDetails';
+import ProductList from './components/ProductList';
+import State from './components/States';
+import Gallery from './components/Gallery';
+import Slider from './components/slider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Router>
+        <Header></Header>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/ProductDetails/:id" component={ProductDetails}></Route>
+        <Route path="/ProductList" component={ProductList}></Route>
+        <Route path="/state" component={State}></Route>
+        <Route path="/Gallery" component={Gallery}></Route>
+        {/* <Route path="/slider" component={Slider}></Route> */}
+      </Router>
+
     </div>
   );
 }
